f73252cf8fd206e865500e04ee9126b55dd0cac1
Abraão Sousa <abraao_andrade_sousa@hotmail.com>
2025-04-06 21:36:16 -0300

    add change name and styling

--- src/app/(components)/auth/LogoutBtn.tsx
+++ src/app/(components)/auth/LogoutBtn.tsx
@@ -1,4 +1,5 @@
 import { logout } from "@/(actions)/auth";
+import { FiLogOut } from "react-icons/fi";
 
 export default function LogoutBtn() {
   const handleLogout = async () => {
@@ -6,8 +7,11 @@ export default function LogoutBtn() {
   };
 
   return (
-    <button className="btn btn-primary" onClick={handleLogout}>
-      Logout
+    <button
+      className="btn btn-primary hover:bg-[#5a62ce] py-2 px-2 rounded-lg"
+      onClick={handleLogout}
+    >
+      <FiLogOut size={20} />
     </button>
   );
 }
--- src/app/(components)/chat/Header.tsx
+++ src/app/(components)/chat/Header.tsx
@@ -1,10 +1,14 @@
 import LogoutBtn from "../auth/LogoutBtn";
+import NameModal from "./NameModal";
 
 const Header = () => {
   return (
     <div className="flex flex-row justify-between m-0 p-[7px] px-20 bg-[#7A80DA]  text-white">
       <h5 className="title text-center text-[35px] ">ChatTalk!</h5>
-      <LogoutBtn />
+      <div className="flex flex-row gap-2 items-center">
+        <NameModal />
+        <LogoutBtn />
+      </div>
     </div>
   );
 };
